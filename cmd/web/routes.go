04f867d61105e7ee1e2d91512131f5520f9e52b9
serhatguzel <sguzel@gtu.edu.tr>
2025-05-03 04:06:11 +0300

    new html and js working

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,5 +19,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
+	fileServer := http.FileServer(http.Dir("./static/"))             // File server for static files
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer)) // Serve static files from the static directory
+
 	return mux
 }
